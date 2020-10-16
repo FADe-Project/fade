18d52038520a6ebeee9e33d59eb12f4a3ea3edc7
ldmsys <admin@ldmsys.net>
2020-10-16 22:52:36 +0900

    chore(stub): detect if dist/ diretory exists

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 console.error("[FADe] main.js is now deprecated, please use yarn start or node dist/");
-require("./dist");
-
+const __$TUB_F$ = require('fs');
+if(__$TUB_F$.existsSync("./dist"))
+	require("./dist");
+else {
+	console.error("[FADe] dist/ not found. FADe was rewritten into typescript. Please do tsc.");
+}
 /*
 
 Whatever you write, i don't care();
